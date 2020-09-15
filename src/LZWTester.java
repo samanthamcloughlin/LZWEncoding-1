9f26ede8c880f6a232bba9d6a2f66283a9fd641e
Luke Riley <lukegough@me.com>
2020-09-15 15:11:21 -0700

    made tester file

---
+++ src/LZWTester.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class LZWTester {
+	public static void main(String[] args) throws IOException {
+		LZWEncoding encoder = new LZWEncoding();
+		encoder.encoding("file2.txt");
+		
+	}
+}
