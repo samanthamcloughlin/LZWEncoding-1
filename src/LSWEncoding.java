78eebe5a74f791fe291ab200a0200c42a45ef9e0
Crypto-Advisor <40330472+Crypto-Advisor@users.noreply.github.com>
2020-09-15 14:42:39 -0700

    adrien changes1
    decoding method reading in dictionary from file

--- src/LSWEncoding.java
+++ src/LSWEncoding.java
@@ -1,19 +1,21 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
 public class LSWEncoding {
+	
+	
 
-	public static void main(String[] args) throws IOException {
+	public void encoding(String input) throws IOException {
 				
-		BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+		BufferedReader br = new BufferedReader(new FileReader(input));
 		String p = "";
 		String c = "" + (char)br.read();
 		String concat = "" + c;
 		
-		//make the dictionary
-		//initialize it for 0-255
 		ArrayList<String> dictionary = new ArrayList<String>();
 		
 		for (int i = 0; i < 256; i++)
@@ -48,5 +50,36 @@ public class LSWEncoding {
 			c = "" + (char)br.read();
 			concat = "" + p + c;
 		}	
+	}
+	
+	public void decoding(String input) throws IOException{
+		BufferedWriter decodeWriter = new BufferedWriter(new FileWriter(input));
+		BufferedReader br2 = new BufferedReader(new FileReader(input));	
+		ArrayList<String> dictionary2 = new ArrayList<String>();
+		char temp = (char)br2.read();
+		String value = "";
+		while(temp != ';') {
+			temp = (char)br2.read();
+		}
+		while(br2.ready()) {//setting up dictionary to decode file
+			while(temp != '=') {//loop to get full length number 
+				value += temp;
+				temp = (char)br2.read();
+			}
+			int len = Integer.parseInt(value);//convert string value to int
+			temp = (char)br2.read();
+			String dictVal = "";
+			for(int i =0;i<len;i++) {//finds full dictionary value
+				dictVal += (char)br2.read();
+			}
+			dictionary2.add(dictVal);	
+		}
+		br2.close();
+		BufferedReader br3 = new BufferedReader(new FileReader(input));
+		while(temp != ';') {
+			while(temp != ',') {
+				
+			}
+		}
 	}
 }
